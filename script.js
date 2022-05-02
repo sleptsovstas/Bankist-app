6a0be312be819409af013e7fac4e57c2bac84c4c
sleptsovstas <sleptsovstas.v@gmail.com>
2022-05-02 23:55:38 +0900

    user login, display UI and welcome messaage, colc balance, calc summary, user transfers money

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
@@ -172,17 +170,3 @@ btnTransfer.addEventListener('click', e => {
     updateUI(currentAccount);
   }
 });
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-///////////////////////////////////////////////
